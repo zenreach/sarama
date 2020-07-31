9c4ce07dedbc7831c932aa4b55b3412830cfbc9e
Chris Bishop <cbishop@greenfieldlabs.ca>
2020-07-31 15:40:35 -0400

    Revert "Removed excessive logging"
    This reverts commit b1495782fe8f551900373d7ecd05afbd39a17a1d.

--- broker.go
+++ broker.go
@@ -760,6 +760,7 @@ func (b *Broker) send(rb protocolBody, promiseResponse bool, responseHeaderVersi
 }
 
 func (b *Broker) sendAndReceive(req protocolBody, res protocolBody) error {
+	Logger.Println("sendAndReceive:", time.Now().UTC)
 	responseHeaderVersion := int16(-1)
 	if res != nil {
 		responseHeaderVersion = res.headerVersion()
@@ -767,6 +768,7 @@ func (b *Broker) sendAndReceive(req protocolBody, res protocolBody) error {
 
 	promise, err := b.send(req, res != nil, responseHeaderVersion)
 	if err != nil {
+		Logger.Println("send err", err, time.Now().UTC)
 		return err
 	}
 
@@ -778,6 +780,7 @@ func (b *Broker) sendAndReceive(req protocolBody, res protocolBody) error {
 	case buf := <-promise.packets:
 		return versionedDecode(buf, res, req.version())
 	case err = <-promise.errors:
+		Logger.Println("read err", err, time.Now().UTC)
 		return err
 	}
 }
