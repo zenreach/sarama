04557d89c98f37c4b03c69f1642a421d8943f767
Chris Bishop <cbishop@greenfieldlabs.ca>
2020-07-31 15:38:58 -0400

    Revert "Hard coded timeout."
    This reverts commit ada6c4ca28b3eed2e08eb921cda53b8b6f6512c2.

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package sarama
 
 import (
+	"encoding/json"
 	"math/rand"
 	"sort"
 	"sync"
@@ -131,8 +132,8 @@ type client struct {
 // be retrieved from any of the given broker addresses, the client is not created.
 func NewClient(addrs []string, conf *Config) (Client, error) {
 	Logger.Println("Initializing new client")
-	Logger.Println("read timeout:", conf.Net.ReadTimeout)
-	conf.Net.ReadTimeout = 120
+	confJson, _ := json.Marshal(conf)
+	Logger.Println(string(confJson))
 	if conf == nil {
 		conf = NewConfig()
 	}
