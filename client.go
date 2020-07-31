0a14cc3c00730ce68b70a97ef5e02005fbfa3b96
Chris Bishop <cbishop@greenfieldlabs.ca>
2020-07-31 14:52:28 -0400

    Used Json to output object

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package sarama
 
 import (
+	"encoding/json"
 	"math/rand"
 	"sort"
 	"sync"
@@ -131,7 +132,8 @@ type client struct {
 // be retrieved from any of the given broker addresses, the client is not created.
 func NewClient(addrs []string, conf *Config) (Client, error) {
 	Logger.Println("Initializing new client")
-	Logger.Println(conf)
+	confJson, _ := json.Marshal(conf)
+	Logger.Println(string(confJson))
 	if conf == nil {
 		conf = NewConfig()
 	}
