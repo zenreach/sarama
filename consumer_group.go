515f708ae540a3fabb8e273c58c187f19186fc53
Chris Bishop <cbishop@greenfieldlabs.ca>
2020-07-31 15:40:27 -0400

    Revert "Fixed logging"
    This reverts commit 0a80628bceba564a6fd3b6c92c95d7c89df149ac.

--- consumer_group.go
+++ consumer_group.go
@@ -330,9 +330,9 @@ func (c *consumerGroup) joinGroupRequest(coordinator *Broker, topics []string) (
 		SessionTimeout: int32(c.config.Consumer.Group.Session.Timeout / time.Millisecond),
 		ProtocolType:   "consumer",
 	}
-	Logger.Println("joinGroupRequest")
+	fmt.Println("joinGroupRequest")
 	if c.config.Version.IsAtLeast(V0_10_1_0) {
-		Logger.Println("if c.config.Version.IsAtLeast(V0_10_1_0) {")
+		fmt.Println("if c.config.Version.IsAtLeast(V0_10_1_0) {")
 		req.Version = 1
 		req.RebalanceTimeout = int32(c.config.Consumer.Group.Rebalance.Timeout / time.Millisecond)
 	}
@@ -347,12 +347,12 @@ func (c *consumerGroup) joinGroupRequest(coordinator *Broker, topics []string) (
 		UserData: userData,
 	}
 	strategy := c.config.Consumer.Group.Rebalance.Strategy
-	Logger.Println("req.AddGroupProtocolMetadata")
+	fmt.Println("req.AddGroupProtocolMetadata")
 	if err := req.AddGroupProtocolMetadata(strategy.Name(), meta); err != nil {
 		fmt.Println("req.AddGroupProtocolMetadata", err)
 		return nil, err
 	}
-	Logger.Println("coordinator.JoinGroup")
+	fmt.Println("coordinator.JoinGroup")
 	return coordinator.JoinGroup(req)
 }
 
